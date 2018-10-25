accf1d1f5b4a5a5e6e1e1e48a6522a56ddfc0591
Boris Sekachev <boris.sekachev@intel.com>
2018-10-25 17:02:24 +0300

    UI

--- cvat/apps/engine/static/engine/js/annotationUI.js
+++ cvat/apps/engine/static/engine/js/annotationUI.js
@@ -55,6 +55,9 @@ function buildAnnotationUI(job, shapeData, loadJobEvent) {
             z_order: job.z_order,
             id: job.jobid
         },
+        task: {
+            id: job.taskid
+        },
         search: {
             value: window.location.search,
 
@@ -194,7 +197,7 @@ function buildAnnotationUI(job, shapeData, loadJobEvent) {
 
     $(window).on('click', function(event) {
         Logger.updateUserActivityTimer();
-        if (['helpWindow', 'settingsWindow'].indexOf(event.target.id) != -1) {
+        if (event.target.classList.contains('modal')) {
             event.target.classList.add('hidden');
         }
     });
